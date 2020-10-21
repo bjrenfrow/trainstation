69977106893bf917cc7079a02acbb3eb2124b3e6
Brandon Renfrow <bjrenfrow@gmail.com>
2020-10-20 17:52:52 -0700

    hello world api

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const { PORT } = process.env;
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
