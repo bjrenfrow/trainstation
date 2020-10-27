47c651f4367bd9871871b834b492dad8a19bfa8d
Brandon Renfrow <bjrenfrow@gmail.com>
2020-10-27 12:19:21 -0700

    simplify Db

--- src/models/db.js
+++ src/models/db.js
@@ -1,56 +1,18 @@
-import nano from 'nano';
+let store = {};
 
-const connection = nano(process.env.COUCHDB_URL);
-
-const TABLE_NAME = 'store';
-
-let store;
-
-export const create = async () => {
-  try {
-    await connection.db.create(TABLE_NAME);
-  } catch (e) {
-    // idempontent function, so discard this error
-    if (e.reason !== 'The database could not be created, the file already exists.') {
-      console.error(e);
-    }
-  }
-  store = connection.use(TABLE_NAME);
-}
-
-const destroy = async () => {
-  if(process.env.NODE_ENV !== 'test') {
-    return console.error('Trying to destroy db outside of test!')
-  }
-
-  try {
-    await connection.db.destroy(TABLE_NAME)
-  } catch (e) {
-    // idempontent function, so discard this error
-    if (e.message === 'Database does not exist') { return; }
-    console.error(e);
-  }
-}
-
-export const reset = async () => {
-  await destroy();
-  await create();
+export const reset = () => {
+  store = {};
 }
 
 // CORE API
-export const set = async (key, value) => {
-  if (!store) { await create(); }
-  await store.insert({value}, key)
+export const set = (key, value) => {
+  store[key] = value;
 };
 
-export const fetch = async (key) => {
-  if (!store) { await create(); }
-  const response = await store.get(key);
-  return response?.value || null;
+export const fetch = (key) => {
+  return store[key] || undefined;
 }
 
-export const keys = async () => {
-  if (!store) { await create(); }
-  const { rows } = await store.list()
-  return rows.map(({ key }) => key);
+export const keys = () => {
+  return Object.keys(store);
 }
